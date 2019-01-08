558faec3b66666630a144ba8b4f4d2cf504e3a86
Samantha Gatt <smfgatt@gmail.com>
2019-01-08 15:50:40 -0500

    Adds pep8 linter Updates vscode settings and fixes yesterday's lecture files to conform to pep8 standards

--- Intro to Python OOP/Classes.py
+++ Intro to Python OOP/Classes.py
@@ -1,14 +1,14 @@
 class Dog:
-  # constructor
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
-    
-  # the repr method is defined in order to return a string
-  # representation of an instance
-  def __repr__(self):
-    # return "{} is {} years old.".format(self.name, self.age)
-    return f'{self.name} is {self.age} years old.'
+    # constructor
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # the repr method is defined in order to return a string
+    # representation of an instance
+    def __repr__(self):
+        # return "{} is {} years old.".format(self.name, self.age)
+        return f'{self.name} is {self.age} years old.'
 
 
 arty = Dog('Arty', 4)
@@ -17,14 +17,14 @@ print(arty)
 
 # the Bulldog class inherits from the Dog class
 class Bulldog(Dog):
-  def __init__(self, name, age, size):
-    super().__init__(name, age)
-    self.size = size
-    
-  # we'll pass name and age to the parent class for it 
-  # to initialize so that we don't have to duplicate code
-  def __repr__(self):
-    return f'{self.name} is a bulldog. It is {self.age} years old and' 
+    def __init__(self, name, age, size):
+        super().__init__(name, age)
+        self.size = size
+
+    # we'll pass name and age to the parent class for it
+    # to initialize so that we don't have to duplicate code
+    def __repr__(self):
+        return f'{self.name} is a bulldog. It is {self.age} years old and'
 
 
 buster = Bulldog('Buster', 9, 2)
--- Intro to Python OOP/Functions.py
+++ Intro to Python OOP/Functions.py
@@ -2,25 +2,27 @@ import sys
 
 # Write a func that counts the number of vowels in an input string
 
+
 def vowel_count(string):
-  # check how many vowels
-  # iterate through the string
-  # create an array of vowels to check against
-  vowels = ['a', 'e', 'i', 'o', 'u']
-  count = 0
-  for char in strings:
-    # check if this char exists in vowels
-    if char in vowels:
-      # need to increment count
-      count += 1
-  return count
+    # check how many vowels
+    # iterate through the string
+    # create an array of vowels to check against
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for char in string:
+        # check if this char exists in vowels
+        if char in vowels:
+            # need to increment count
+            count += 1
+    return count
+
 
 def simple_vowel_count(s):
-  return sum(char in 'aeiou' for char in s.lower())
+    return sum(char in 'aeiou' for char in s.lower())
 
 l = len(sys.argv)
 
 if l != 2:
-  print('usage: python_demo.py [word]')
+    print('usage: python_demo.py [word]')
 else:
-  print(simple_vowel_count(sys.argv[1]))
+    print(simple_vowel_count(sys.argv[1]))
